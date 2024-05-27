89da276ae7ed58628261ab7b58d06201d48b510b
Parms221 <willy_g29@outlook.com>
2024-05-27 12:22:37 -0500

    :rocket: first commit

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import "jsvectormap/dist/css/jsvectormap.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import React from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>
+        <div className="dark:bg-boxdark-2 dark:text-bodydark min-h-screen">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
