ab7e0034785bf30e26dfbdab72656a8d86afde4e
Hiroshi <ebruno.hiroshi@gmail.com>
2024-06-03 12:13:17 -0500

    add: spring backend

---
+++ gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/PlataformaDeGestionDeProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.arcticcuyes.gestion_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeGestionDeProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeGestionDeProyectosApplication.class, args);
+	}
+
+}
