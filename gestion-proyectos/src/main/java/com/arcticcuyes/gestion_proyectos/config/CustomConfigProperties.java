4daca907debe7dbc3c917ec3bea4cae06d685c21
Hiroshi <ebruno.hiroshi@gmail.com>
2024-06-17 00:54:10 -0500

    add imports in CustomConfigProperties

--- gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/config/CustomConfigProperties.java
+++ gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/config/CustomConfigProperties.java
@@ -1,5 +1,9 @@
 package com.arcticcuyes.gestion_proyectos.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
 @Configuration
 @PropertySources({
     @PropertySource("classpath:custom.properties")
