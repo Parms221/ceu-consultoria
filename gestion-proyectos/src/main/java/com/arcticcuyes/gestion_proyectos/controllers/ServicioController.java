9967ca37d1bb05341f15e3902bc9139cd279b1d0
Piero Varas <varaspiero07@gmail.com>
2024-06-19 12:13:25 -0500

    feat: Service for Servicio, add: validation Servicio

--- gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/controllers/ServicioController.java
+++ gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/controllers/ServicioController.java
@@ -3,11 +3,14 @@ package com.arcticcuyes.gestion_proyectos.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,150 +22,65 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.arcticcuyes.gestion_proyectos.dto.EntregableServicioDTO;
 import com.arcticcuyes.gestion_proyectos.dto.ServicioDTO;
+import com.arcticcuyes.gestion_proyectos.models.ClienteNatural;
 import com.arcticcuyes.gestion_proyectos.models.EntregableServicio;
 import com.arcticcuyes.gestion_proyectos.models.Servicio;
 import com.arcticcuyes.gestion_proyectos.repositories.EntregableServicioRepository;
 import com.arcticcuyes.gestion_proyectos.repositories.ServicioRepository;
+import com.arcticcuyes.gestion_proyectos.services.ServicioService;
+
+import jakarta.validation.Valid;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/servicio")
+@RequestMapping("/servicios")
 public class ServicioController {
 
     @Autowired
-    ServicioRepository servicioRepository;
+    ServicioService servicioService;
 
-    @Autowired
-    EntregableServicioRepository entregablesRepository;
-
-    @GetMapping("/get")
-    public ResponseEntity<?> getServicios() {
-        List<Servicio> servicios = (List<Servicio>) servicioRepository.findAll();
-        if (servicios.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(servicios, HttpStatus.OK);
+    @GetMapping("/")
+    public ResponseEntity<Page<Servicio>> getAllServicios(Pageable pageable) {
+        Page<Servicio> page = servicioService.findAll(pageable);
+        return ResponseEntity.ok(page);
     }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<?> getServicios(@PathVariable Long id) {
+    @PostMapping("/addServicio")
+    public ResponseEntity<?> createServicio(@Valid @RequestBody ServicioDTO servicioDTO, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            // Recopila todos los errores de validación en un mapa
+            Map<String, String> errors = bindingResult.getFieldErrors().stream().collect(Collectors.toMap(
+                            fieldError -> fieldError.getField(),
+                            fieldError -> fieldError.getDefaultMessage()
+                    ));
+
+            // Devuelve la respuesta con los errores en formato JSON y estado 400 Bad Request
+            return ResponseEntity.badRequest().body(errors);
+        }
         try {
-            Optional<Servicio> optionalServicio = servicioRepository.findById(id);
-            if (optionalServicio.isPresent()) {
-                Servicio servicio = optionalServicio.get();
-                return new ResponseEntity<>(servicio, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>("Servicio no encontrado", HttpStatus.NOT_FOUND);
-            }
+            Servicio createdServicio = servicioService.saveServicio(servicioDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdServicio);
         } catch (Exception e) {
-            return new ResponseEntity<>("Error al obtener el servicio: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al crear el servicio: " + e.getMessage());
         }
     }
-
     
-
-    @PostMapping("/post")
-    public ResponseEntity<?> saveServicio(@RequestBody ServicioDTO servicioDTO) {
-        try {
-            // Crear un nuevo objeto Servicio a partir de ServicioDTO
-            Servicio servicio = new Servicio();
-            servicio.setTitulo(servicioDTO.getTitulo());
-            servicio.setPrecio(servicioDTO.getPrecio());
-            servicio.setDescripcion(servicioDTO.getDescripcion());
-
-            Servicio savedServicio = servicioRepository.save(servicio);
-
-            // Guardar los EntregableServicio asociados
-            List<EntregableServicio> entregables = new ArrayList<>();
-            if (servicioDTO.getEntregablesServicio() != null) {
-                for (EntregableServicioDTO entregableDTO : servicioDTO.getEntregablesServicio()) {
-                    // Crear el objeto EntregableServicio y establecer la relación bidireccional
-                    EntregableServicio entregable = new EntregableServicio();
-                    entregable.setTitulo(entregableDTO.getTitulo());
-                    entregable.setDescripcion(entregableDTO.getDescripcion());
-                    entregable.setServicio(savedServicio); // Establecer la relación con el servicio persistido
-
-                    // Guardar cada entregable
-                    EntregableServicio savedEntregable = entregablesRepository.save(entregable);
-                    entregables.add(savedEntregable);
-                }
-            }
-
-            // Establecer la lista de entregables en el servicio
-            savedServicio.setEntregablesDelServicio(entregables);
-
-            return new ResponseEntity<>(savedServicio, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error al guardar el servicio: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping("/getServicio/{id}")
+    public ResponseEntity<Servicio> getServicioById(@PathVariable Long id) {
+        Servicio servicio = servicioService.findServicioById(id);
+        return ResponseEntity.ok(servicio);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<?> updateServicio(@PathVariable Long id, @RequestBody ServicioDTO servicioDTO) {
-        try {
-            // Verificar si el servicio existe
-            if (!servicioRepository.existsById(id)) {
-                return new ResponseEntity<>("Servicio no encontrado", HttpStatus.NOT_FOUND);
-            }
-
-            // Obtener el servicio existente
-            Servicio existingServicio = servicioRepository.findById(id).orElse(null);
-
-            // Actualizar los campos del servicio
-            existingServicio.setTitulo(servicioDTO.getTitulo());
-            existingServicio.setPrecio(servicioDTO.getPrecio());
-            existingServicio.setDescripcion(servicioDTO.getDescripcion());
-
-            // Manejar los EntregableServicio asociados
-            List<EntregableServicio> updatedEntregables = new ArrayList<>();
-            if (servicioDTO.getEntregablesServicio() != null) {
-                for (EntregableServicioDTO entregableDTO : servicioDTO.getEntregablesServicio()) {
-                    EntregableServicio entregable;
-                    if (entregableDTO.getId() != null) {
-                        // Actualizar el entregable existente
-                        entregable = entregablesRepository.findById(entregableDTO.getId()).get();
-                    } else {
-                        // Crear un nuevo entregable
-                        entregable = new EntregableServicio();
-                    }
-                    entregable.setTitulo(entregableDTO.getTitulo());
-                    entregable.setDescripcion(entregableDTO.getDescripcion());
-                    entregable.setServicio(existingServicio);
-
-                    // Guardar el entregable
-                    EntregableServicio savedEntregable = entregablesRepository.save(entregable);
-                    updatedEntregables.add(savedEntregable);
-                }
-            }
-
-            // Guardar el servicio actualizado
-            Servicio updatedServicio = servicioRepository.save(existingServicio);
-
-            // Eliminar los entregables que ya no están en la lista actualizada
-            List<EntregableServicio> currentEntregables = existingServicio.getEntregablesDelServicio();
-            
-            for (EntregableServicio entregable : currentEntregables) {
-                if (!updatedEntregables.contains(entregable)) {
-                    entregablesRepository.delete(entregable);
-                }
-            }
-            return new ResponseEntity<>(updatedServicio, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error al actualizar el servicio: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @PutMapping("/updateServicio/{id}")
+    public ResponseEntity<Servicio> updateServicio(
+            @PathVariable Long id,
+            @RequestBody ServicioDTO servicioDTO) {
+        
+        Servicio updatedServicio = servicioService.updateServicio(id, servicioDTO);
+        return ResponseEntity.ok(updatedServicio);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<?> deleteServicio(@PathVariable Long id) {
-        try {
-            // Verificar si el servicio existe
-            if (!servicioRepository.existsById(id)) {
-                return new ResponseEntity<>("Servicio no encontrado", HttpStatus.NOT_FOUND);
-            }
-            // Eliminar el servicio y sus entregables asociados
-            servicioRepository.deleteById(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Error al eliminar el servicio: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
 }
--- gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/dto/EntregableServicioDTO.java
+++ gestion-proyectos/src/main/java/com/arcticcuyes/gestion_proyectos/dto/EntregableServicioDTO.java
@@ -1,5 +1,6 @@
 package com.arcticcuyes.gestion_proyectos.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EntregableServicioDTO {
+
     private Long id;
+    
+    @NotEmpty(message = "El título del entregable es obligatorio")
     private String titulo;
+
+    @NotEmpty(message = "La descripción del entregable es obligatoria")
     private String descripcion;
+
 
 }
